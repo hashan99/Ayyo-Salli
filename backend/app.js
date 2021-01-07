ae89e214cb64c1e1ec97b2eb2cfde0ca524c45bd
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-07 15:12:13 +0530

    add some env variables

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 //initial route
-app.get('/', (req,res)=>{
+app.get(api+'/products', (req,res)=>{
     res.send('Ayyo Salli');
 })
 
 app.listen(3000, ()=>{
+    console.log(api);
     console.log('server is running http://localhost:3000');
 })
