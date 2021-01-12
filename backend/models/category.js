bfce1b668caf37712e6dded8b20bdcf9fe1ba58d
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-12 13:54:27 +0530

    categorySchema created

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 //Category Schema
 const categorySchema = mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+    },
+    colors: {
+        type: String,
+    }
 })
 
 //Category Model
